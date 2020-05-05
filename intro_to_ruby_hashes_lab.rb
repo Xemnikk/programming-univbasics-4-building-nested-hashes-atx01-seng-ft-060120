167d31624618e08cf3c009a9c2f7f6bba96e732c
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-05 20:52:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,10 +36,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one_piece_owned => "25",
-        :two_pieces_owned => "50",
-        :three_pieces_owned => "100",
-        :four_pieces_owned => "200"
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
       },
       :names => {
         :reading_railroad => {},
@@ -73,13 +73,13 @@ def monopoly_with_fourth_tier
           :mortgage => 100
         },
         :pennsylvania_railroad => {
-          :mortgage => "200"
+          :mortgage => 200
         },
         :b_and_o_railroad => {
-          :mortgage => "400"
+          :mortgage => 400
         },
         :shortline_railroad => {
-          :mortgage => "800"
+          :mortgage => 800
         },
       }
     }
